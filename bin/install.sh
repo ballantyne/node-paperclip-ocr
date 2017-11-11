94001403de60354eb65c620ddfe9f4083a2d8393
Scott Ballantyne <ussballantyne@gmail.com>
2017-11-10 19:52:31 -0600

    initial commit

---
+++ bin/install.sh
@@ -0,0 +1,13 @@
+sudo apt-get install libleptonica-dev
+sudo apt-get install g++ # or clang++ (presumably)
+sudo apt-get install autoconf automake libtool
+sudo apt-get install autoconf-archive
+sudo apt-get install pkg-config
+sudo apt-get install libpng12-dev
+sudo apt-get install libjpeg8-dev
+sudo apt-get install libtiff5-dev
+sudo apt-get install zlib1g-dev
+sudo apt-get install libicu-dev
+sudo apt-get install libpango1.0-dev
+sudo apt-get install libcairo2-dev
+sudo apt-get install libtesseract-dev
