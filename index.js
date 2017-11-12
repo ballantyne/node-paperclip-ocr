93c6b09cc99743714ea21d535f7fbf074f6ddfac
Scott Ballantyne <ussballantyne@gmail.com>
2017-11-12 13:20:04 -0600

    Making this module just directly connect to the tesseract module

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const tesseract           = require('tesseract_native');
-const Ocr                 = new tesseract.OcrEio();
-
-module.exports            = function(paperclip) {
-  var obj = {};
-  obj.paperclip           = paperclip;
-  obj.perform             = function(options, next) {
-    var attribute;
-
-    if (options.attribute) {
-      attribute           = options.attribute;
-    } else {
-      attribute           = 'text';
-    }
-    
-    Ocr.ocr(this.paperclip.file().file.buffer, function(err, result) {
-      var object          = {};
-      if (result) {
-        object[attribute] = result;
-      }
-      next(err, object);
-    });
-  }
-  return obj;
-}
+const tesseract           = require('node-tesseract-paperclip');
+module.exports            = tesseract;
 
